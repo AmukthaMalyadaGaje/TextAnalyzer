e76ad1c5855af63a3a74d39ff905439a529a6ef1
AmukthaMalyadaGaje <amukthagaje@gmail.com>
2023-11-04 12:17:32 +0530

    Project files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import About from './components/About';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+  // Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div>
+      <BrowserRouter>
+
+
+        <Routes>
+          <Route path="/" element={<><Navbar title='TextUtils' vis={true}/><Textform /></>} />
+          <Route path="/about" element={<><Navbar title='TextUtils' vis = {false}/><About /></>} />
+          {/* <Route path="/" element={} /> */}
+          {/* <Route path="/about" element={<About />} /> */}
+        </Routes>
+
+      </BrowserRouter>
   );
 }
 
